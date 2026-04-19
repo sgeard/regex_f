412f5d351d7b98ec7681ff4c39ea219b5ffd8f33
sgeard <simon@whiteowl.co.uk>
2026-04-19 10:59:40 +0100

    Add support for user-defined comment in git upload

--- export_to_gh.sh
+++ export_to_gh.sh
@@ -5,7 +5,15 @@ git clone git@github.com:sgeard/regex_f.git $tmpdir
 svn export --force svn://persephone/projects/tcl_re $tmpdir
 cd $tmpdir
 git add -A
-git commit -m "Sync from SVN r$(svnversion ..)"
+
+if [[ $# == 1 ]]; then
+    comment=$1
+else
+    default_comment="Sync from SVN r$(svnversion ..)"
+    read -p "Comment [$default_comment] > " comment
+    comment=${comment:-$default_comment}
+fi
+git commit -m "$comment"
 git push
 
 cd ..
